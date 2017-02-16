62eb62e8abed91b8ee73da303912e8c3f273281e
Erik Hedberg <erik@gish.se>
2017-02-16 10:38:43 +0100

    Add base for showing who's currently in debt

--- src/Models.elm
+++ src/Models.elm
@@ -9,21 +9,13 @@ type alias Debt =
   , sender : String
   }
 
+
 type alias Model =
   { debts : List Debt
   }
 
-initialDebt : Debt
-initialDebt =
-  { amount = 10
-  , created_at = 20
-  , description = "A car"
-  , id = "13"
-  , receiver = "12"
-  , sender = "11"
-  }
 
 initialModel : Model
 initialModel =
-  { debts = [initialDebt]
+  { debts = []
   }
--- src/View.elm
+++ src/View.elm
@@ -2,6 +2,8 @@ module View exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (class)
+import List exposing (head, tail, foldr, sum, filter, reverse)
+import List.Extra exposing (unique)
 import Models exposing (Model, Debt)
 import Messages exposing (Msg(..))
 
@@ -9,9 +11,63 @@ view : Model -> Html Msg
 view model =
   div [ class "container" ]
     [ div [ class "col-xs-12" ]
-      [ h3 [ ] [ text "Transaktioner" ]
+      [ h3 [] [ text "Status" ]
+      , status model.debts
+      , h3 [] [ text "Transaktioner" ]
       , transactionTable model.debts ]
     ]
+
+
+status : List Debt -> Html Msg
+status debts =
+  p []
+    [ text (getUserInDebt debts)
+    , text (toString (getHighestDebt debts))
+    ]
+
+
+getDebtByUserId : List Debt -> String -> Int
+getDebtByUserId debts userId =
+  let
+    matchesId : String -> Debt -> Bool
+    matchesId userId debt =
+      userId == debt.receiver
+  in
+  filter (matchesId userId) debts
+    |> List.map .amount
+    |> sum
+
+
+getHighestDebt : List Debt -> Int
+getHighestDebt debts =
+  getUniqueUserIds debts
+  |> List.map (getDebtByUserId debts)
+  |> foldr (-) 0
+  |> abs
+
+
+getUniqueUserIds : List Debt -> List String
+getUniqueUserIds debts =
+  List.map .receiver debts
+  |> unique
+
+
+getUserInDebt : List Debt -> String
+getUserInDebt debts =
+  let
+    idUserA =
+      head (getUniqueUserIds debts)
+      |> Maybe.withDefault ""
+    idUserB =
+      head (reverse (getUniqueUserIds debts))
+      |> Maybe.withDefault ""
+    debtUserA = getDebtByUserId debts idUserA
+    debtUserB = getDebtByUserId debts idUserB
+  in
+    if (debtUserA > debtUserB) then
+      idUserA
+    else
+      idUserB
 
 
 transactionTable : List Debt -> Html Msg
